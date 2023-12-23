fa26e98b9284f406e77414f051f8421dcec44f2a
antoni668 <antoni668@mail.ru>
2023-12-23 12:26:38 +0600

    parsing input string

--- main.go
+++ main.go
@@ -4,7 +4,26 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 )
+
+func parseInputString(inputString string) (string, string, string, error) {
+	reArabic := regexp.MustCompile(`^\s*(\d+)\s*([-+*/])\s*(\d+)\s*$`)
+	matchArabic := reArabic.FindStringSubmatch(inputString)
+
+	if matchArabic != nil {
+		return matchArabic[1], matchArabic[2], matchArabic[3], nil
+	}
+
+	reRoman := regexp.MustCompile(`^\s*([IVX]+)\s*([-+*/])\s*([IVX]+)\s*$`)
+	matchRoman := reRoman.FindStringSubmatch(inputString)
+
+	if matchRoman != nil {
+		return matchRoman[1], matchRoman[2], matchRoman[3], nil
+	}
+
+	return "", "", "", fmt.Errorf("введенные данные не соответствуют ожидаемому формату")
+}
 
 func main() {
 	fmt.Print("Введите строку: ")
@@ -12,6 +31,16 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	if scanner.Scan() {
 		inputString := scanner.Text()
-		fmt.Printf("Вы ввели: %s\n", inputString)
+
+		num1, operator, num2, err := parseInputString(inputString)
+
+		if err != nil {
+			fmt.Println("Ошибка:", err)
+			return
+		}
+
+		fmt.Printf("Первое число: %s\n", num1)
+		fmt.Printf("Оператор: %s\n", operator)
+		fmt.Printf("Второе число: %s\n", num2)
 	}
 }
