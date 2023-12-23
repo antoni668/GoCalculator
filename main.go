2be064d12b53dc02b071f84c80deddeb4a8c6db3
antoni668 <antoni668@mail.ru>
2023-12-23 17:17:49 +0600

    add arithmeticOperation

--- main.go
+++ main.go
@@ -8,25 +8,27 @@ import (
 	"strconv"
 )
 
-func parseInputString(inputString string) (string, string, string, error) {
+func parseInputString(inputString string) (bool, string, string, string, error) {
 	reArabic := regexp.MustCompile(`^\s*(\d+)\s*([-+*/])\s*(\d+)\s*$`)
 	matchArabic := reArabic.FindStringSubmatch(inputString)
+	isArabic := true
 
 	if matchArabic != nil {
-		return matchArabic[1], matchArabic[2], matchArabic[3], nil
+		return isArabic, matchArabic[1], matchArabic[2], matchArabic[3], nil
 	}
 
 	reRoman := regexp.MustCompile(`^\s*([IVX]+)\s*([-+*/])\s*([IVX]+)\s*$`)
 	matchRoman := reRoman.FindStringSubmatch(inputString)
 
 	if matchRoman != nil {
-		return matchRoman[1], matchRoman[2], matchRoman[3], nil
+		isArabic = false
+		return isArabic, matchRoman[1], matchRoman[2], matchRoman[3], nil
 	}
 
-	return "", "", "", fmt.Errorf("введенные данные не соответствуют ожидаемому формату")
+	return false, "", "", "", fmt.Errorf("введенные данные не соответствуют ожидаемому формату")
 }
 
-func romanToArabic(roman string) int {
+func toArabic(roman string) int {
 	regex := regexp.MustCompile("^[0-9]+$")
 
 	if regex.MatchString(roman) {
@@ -60,7 +62,7 @@ func romanToArabic(roman string) int {
 	return result
 }
 
-func arabicToRoman(arabic int) string {
+func toRoman(arabic int) string {
 	arabicNumerals := []int{100, 90, 50, 40, 10, 9, 5, 4, 1}
 	romanNumerals := []string{"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
 
@@ -79,6 +81,27 @@ func isInRange(number int) bool {
 	return number >= 1 && number <= 10
 }
 
+func isPositive(number int) bool {
+	return number >= 1
+}
+
+func arithmeticOperation(num1, num2 int, op string) int {
+	var result int
+
+	switch op {
+	case "+":
+		result = num1 + num2
+	case "-":
+		result = num1 - num2
+	case "*":
+		result = num1 * num2
+	case "/":
+		result = num1 / num2
+	}
+
+	return result
+}
+
 func main() {
 	fmt.Print("Введите строку: ")
 
@@ -86,20 +109,30 @@ func main() {
 	if scanner.Scan() {
 		inputString := scanner.Text()
 
-		num1, operator, num2, err := parseInputString(inputString)
+		isArabic, num1, operator, num2, err := parseInputString(inputString)
 
 		if err != nil {
 			fmt.Println("Ошибка:", err)
 			return
 		}
 
-		if !isInRange(romanToArabic(num1)) || !isInRange(romanToArabic(num2)) {
+		if !isInRange(toArabic(num1)) || !isInRange(toArabic(num2)) {
 			fmt.Println("Ошибка: число выходит за пределы допустимого диапазона")
 			return
 		}
 
-		fmt.Printf("Первое число: %s\n", num1)
-		fmt.Printf("Оператор: %s\n", operator)
-		fmt.Printf("Второе число: %s\n", num2)
+		result := arithmeticOperation(toArabic(num1), toArabic(num2), operator)
+
+		if !isArabic {
+			if isPositive(result) {
+				fmt.Printf("Результат: %v\n", toRoman(result))
+				return
+			} else {
+				fmt.Println("Ошибка: результат меньше I")
+				return
+			}
+		}
+
+		fmt.Printf("Результат: %v\n", result)
 	}
 }
