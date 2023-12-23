532b0bb6f564fcfea222257ccba7bf82d6b37354
antoni668 <antoni668@mail.ru>
2023-12-23 14:17:48 +0600

    add converters arabicToRoman and RomanToArabic

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
 )
 
 func parseInputString(inputString string) (string, string, string, error) {
@@ -23,6 +24,55 @@ func parseInputString(inputString string) (string, string, string, error) {
 	}
 
 	return "", "", "", fmt.Errorf("введенные данные не соответствуют ожидаемому формату")
+}
+
+func romanToArabic(roman string) int {
+	regex := regexp.MustCompile("^[0-9]+$")
+
+	if regex.MatchString(roman) {
+		num, _ := strconv.Atoi(roman)
+		return num
+	}
+
+	romanNumerals := map[string]int{
+		"I": 1,
+		"V": 5,
+		"X": 10,
+		"L": 50,
+		"C": 100,
+	}
+
+	result := 0
+	prevValue := 0
+
+	for i := len(roman) - 1; i >= 0; i-- {
+		value := romanNumerals[string(roman[i])]
+
+		if value < prevValue {
+			result -= value
+		} else {
+			result += value
+		}
+
+		prevValue = value
+	}
+
+	return result
+}
+
+func arabicToRoman(arabic int) string {
+	arabicNumerals := []int{100, 90, 50, 40, 10, 9, 5, 4, 1}
+	romanNumerals := []string{"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+
+	result := ""
+	for i := 0; i < len(arabicNumerals); i++ {
+		for arabic >= arabicNumerals[i] {
+			result += romanNumerals[i]
+			arabic -= arabicNumerals[i]
+		}
+	}
+
+	return result
 }
 
 func main() {
